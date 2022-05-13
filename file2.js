556556cb121eed4ecb280f108d355bfd97dc2383
nizamuddins <uddinmdnizam61@gmail.com>
2022-05-13 10:58:07 +0530

    complete

---
+++ file2.js
@@ -0,0 +1,19 @@
+"use strict";
+// intro to variables
+
+// variables can store some information
+// we can use that information later
+// we can change that information later
+
+// declare a variable 
+
+var firstName = "nziam";
+
+// use a variable 
+console.log(firstName);
+
+// change value 
+
+firstName = "nayeem";
+
+console.log(firstName);
---
+++ file4.js
@@ -0,0 +1,7 @@
+// let keyword 
+// declare variable with let keyword 
+
+let firstName = "nizam";
+firstName = "nizam";
+console.log(firstName);
+
